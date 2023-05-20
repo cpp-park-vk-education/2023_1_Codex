a2e11c1d1a976a4ce1a09cee26ed68c960e45398
Owling797 <zavadskayalus@gmail.com>
2023-05-20 21:53:23 +0300

    Revert "remove old files"
    This reverts commit b4dcd7a2575ebf6268ee9862b24d078594e64d62.

---
+++ client/include/mainwindow.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QMainWindow>
+#include <memory>
+
+#include "Client.hpp"
+#include "ProblemTypeWindow.hpp"
+#include "UI_MainWindow.hpp"
+#include "UsageWindow.hpp"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+   public:
+    MainWindow(const Client::ClientSPtr& client, QWidget* parent = NULL);
+    // MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+    void setClient(const Client::ClientSPtr& client);
+   private slots:
+    // Слоты от кнопок главного окна
+    void on_usageButton_clicked();
+    void on_startButton_clicked();
+
+   signals:
+    void clientAvailable(const Client::ClientSPtr& client);
+
+   private:
+    Ui::MainWindow* ui;
+    std::shared_ptr<ProblemTypeWindow> problemTypeWindow;
+    std::shared_ptr<UsageWindow> usageWindow;
+    Client::ClientSPtr m_client;
+};
