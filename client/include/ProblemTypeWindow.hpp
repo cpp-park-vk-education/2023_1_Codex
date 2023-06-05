800d26ee9ab76f1698a7e8e6cfd312c0bd8f7970
Owling797 <62678416+Owling797@users.noreply.github.com>
2023-06-05 06:20:38 +0300

    clear unneeded code

--- client/include/ProblemTypeWindow.hpp
+++ client/include/ProblemTypeWindow.hpp
@@ -6,7 +6,6 @@
 
 #include "SolutionWindow.hpp"
 #include "UI_PproblemTypeWindow.hpp"
-//#include "problemType.hpp"
 
 #include "Client.hpp"
 #include "TaskInfo.hpp"
@@ -21,7 +20,6 @@ class ProblemTypeWindow : public QMainWindow {
     Q_OBJECT
    public:
     ProblemTypeWindow(const Client::ClientSPtr& client, QWidget* parent = nullptr);
-    // ProblemTypeWindow(QWidget* parent = nullptr);
     ~ProblemTypeWindow();
 
    signals:
@@ -35,21 +33,12 @@ class ProblemTypeWindow : public QMainWindow {
     void onBackButtonClicked();
 
    private:
-    // QString TaskTypesHandlersNames[taskTypesNum];
     Ui::ProblemTypeWindow* ui;
     std::shared_ptr<SolutionWindow> solutionWindow;
     QString typeName;
     Client::TaskTypes taskType;
-    // QVBoxLayout* typesChoosingLayout;
     QPushButton* backButton;
     std::shared_ptr<QLabel> chooseTypeLabel;
-    /*QMenu* menuArray[TaskTypesMap.size()]; // глобальные типы задач
-    QVector<QAction *> actions[TaskTypesMap.size()]; // конкретные типы задач
-    QPushButton *buttons[TaskTypesMap.size()]; // кнопки выпадающих меню
-    */
-    /*QVector<QMenu*> menuArray;
-    QVector<QVector<QAction*>> actions;
-    QVector<QPushButton*> buttons;*/
 
     Client::ClientSPtr m_client;
 
